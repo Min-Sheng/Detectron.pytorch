96457185c42c6fdb225d1c3cf990485c445954b5
f9979a77-0259-4e25-a644-dbbf72752875 <vincentwu929@ln01-twnia2.twcc.ai>
2020-03-01 12:03:31 +0800

    Revise bash files

--- test.sh
+++ test.sh
@@ -1,4 +1,31 @@
-CUDA_VISIBLE_DEVICES=0 python tools/test_net_few_shot.py \
-                        --dataset fss_cell --cfg configs/few_shot/e2e_mask_rcnn_R-50-C4_1x.yaml \
-                        --load_ckpt Outputs3/e2e_mask_rcnn_R-50-C4_1x/Feb12-04-50-20_32b15552e77a_step/ckpt/model_step17999.pth \
+module load gnu7
+module load nvidia/cuda/10.1
+
+srun --account=MST107266 --gres=gpu:1 --cpus-per-task=4 python3 -u tools/test_net_few_shot.py \
+                        --dataset fss_cell \
+                        --load_ckpt Outputs/e2e_mask_rcnn_R-50-FPN_1x_1_5shot/Feb29-20-15-52_gn1110.twcc.ai/ckpt/model_5shot_epoch49_step23.pth \
+						--g 1 --seen 2 --k 5  --a 5 --vis
+"""
+srun --account=MST107266 --gres=gpu:1 --cpus-per-task=4 python3 -u tools/test_net_few_shot.py \
+                        --dataset fss_cell \
+                        --load_ckpt Outputs/e2e_mask_rcnn_R-50-C4_1x_2/Feb20-02-34-56_gn1101.twcc.ai_step/ckpt/model_step9999.pth \
+						--g 2 --seen 2 --a 5 \
                         #--vis
+
+srun --account=MST107266 --gres=gpu:1 --cpus-per-task=4 python3 -u tools/test_net_few_shot.py \
+                        --dataset fss_cell \
+                        --load_ckpt Outputs/e2e_mask_rcnn_R-50-C4_1x_3/Feb20-04-53-36_gn1118.twcc.ai_step/ckpt/model_step9999.pth \
+						--g 3 --seen 2 --a 5 \
+                        #--vis
+
+srun --account=MST107266 --gres=gpu:1 --cpus-per-task=4 python3 -u tools/test_net_few_shot.py \
+                        --dataset fss_cell \
+                        --load_ckpt Outputs/e2e_mask_rcnn_R-50-C4_1x_4/Feb20-07-24-28_gn1101.twcc.ai_step/ckpt/model_step9999.pth \
+						--g 4 --seen 2 --a 5 \
+                        #--vis
+srun --account=MST107266 --gres=gpu:1 --cpus-per-task=4 python3 -u tools/test_net_few_shot.py \
+                        --dataset fss_cell \
+                        --load_ckpt Outputs/e2e_mask_rcnn_R-50-C4_1x_5/Feb20-09-59-16_gn1101.twcc.ai_step/ckpt/model_step9999.pth \
+						--g 5 --seen 2 --a 5 \
+                        #--vis
+"""
--- train.sh
+++ train.sh
@@ -1,5 +1,72 @@
-CUDA_VISIBLE_DEVICES=0 python tools/train_net_step.py \
-				   --dataset fss_cell --cfg configs/few_shot/e2e_mask_rcnn_R-50-C4_1x.yaml \
-				   --use_tfboard \
-                   --bs 1 --nw 8 \
-				   --g 1 --seen 1
+module load gnu7
+module load nvidia/cuda/10.1
+
+srun --account=MST107266 --gres=gpu:2 --cpus-per-task=8 python3 -u tools/train_net.py \
+                        --dataset fss_cell \
+                        --lr_decay_epochs 20 30 40\
+                        --use_tfboard --bs 64 --nw 8 \
+						--g 1 --seen 1 --k 5\
+                        --epochs 50
+
+srun --account=MST107266 --gres=gpu:4 --cpus-per-task=16 python3 -u tools/train_net.py \
+                        --dataset fss_cell \
+                        --lr_decay_epochs 20 30 40\
+                        --use_tfboard --bs 64 --nw 16 \
+						--g 2 --seen 1 --k 5\
+                        --epochs 50
+
+srun --account=MST107266 --gres=gpu:4 --cpus-per-task=16 python3 -u tools/train_net.py \
+                        --dataset fss_cell \
+                        --lr_decay_epochs 20 30 40\
+                        --use_tfboard --bs 64 --nw 16 \
+						--g 3 --seen 1 --k 5\
+                        --epochs 50
+
+srun --account=MST107266 --gres=gpu:4 --cpus-per-task=16 python3 -u tools/train_net.py \
+                        --dataset fss_cell \
+                        --lr_decay_epochs 20 30 40\
+                        --use_tfboard --bs 64 --nw 16 \
+						--g 4 --seen 1 --k 5\
+                        --epochs 50
+
+srun --account=MST107266 --gres=gpu:4 --cpus-per-task=16 python3 -u tools/train_net.py \
+                        --dataset fss_cell \
+                        --lr_decay_epochs 20 30 40\
+                        --use_tfboard --bs 64 --nw 16 \
+						--g 5 --seen 1 --k 5\
+                        --epochs 50
+
+srun --account=MST107266 --gres=gpu:4 --cpus-per-task=16 python3 -u tools/train_net.py \
+                        --dataset fss_cell \
+                        --lr_decay_epochs 20 30 40\
+                        --use_tfboard --bs 64 --nw 16 \
+						--g 1 --seen 1 --k 1\
+                        --epochs 50
+
+srun --account=MST107266 --gres=gpu:4 --cpus-per-task=16 python3 -u tools/train_net.py \
+                        --dataset fss_cell \
+                        --lr_decay_epochs 20 30 40\
+                        --use_tfboard --bs 64 --nw 16 \
+						--g 2 --seen 1 --k 1\
+                        --epochs 50
+
+srun --account=MST107266 --gres=gpu:4 --cpus-per-task=16 python3 -u tools/train_net.py \
+                        --dataset fss_cell \
+                        --lr_decay_epochs 20 30 40\
+                        --use_tfboard --bs 64 --nw 16 \
+						--g 3 --seen 1 --k 1\
+                        --epochs 50
+
+srun --account=MST107266 --gres=gpu:4 --cpus-per-task=16 python3 -u tools/train_net.py \
+                        --dataset fss_cell \
+                        --lr_decay_epochs 20 30 40\
+                        --use_tfboard --bs 64 --nw 32 \
+						--g 4 --seen 1 --k 1\
+                        --epochs 50
+
+srun --account=MST107266 --gres=gpu:4 --cpus-per-task=16 python3 -u tools/train_net.py \
+                        --dataset fss_cell \
+                        --lr_decay_epochs 20 30 40\
+                        --use_tfboard --bs 64 --nw 16 \
+						--g 5 --seen 1 --k 1\
+                        --epochs 50
